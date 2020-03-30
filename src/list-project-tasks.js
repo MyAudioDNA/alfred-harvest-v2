bdb0b14346f0d020e1cbb2be04edbbc97502bdd7
Keith Frey <keith@Keiths-MacBook-Pro.local>
2020-03-29 20:46:07 -0700

    Replace projects endpoint with project_asinments

--- src/list-project-tasks.js
+++ src/list-project-tasks.js
@@ -3,30 +3,25 @@ import { apiCall } from './utils/helpers';
 const alfy = require('alfy');
 
 const vars = process.env;
-const { projectId } = vars;
+const { projectId, projectTasks } = vars;
 
 const url = `https://api.harvestapp.com/v2/projects/${projectId}/task_assignments`;
 
-await apiCall(url, 'GET')
-    .then(response => {
-        const items = alfy
-            .inputMatches(response.task_assignments, 'task.name')
-            .filter(element => element.is_active)
-            .map(element => ({
-                uid: element.id,
-                title: element.task.name,
-                subtitle: 'Start this task',
-                variables: {
-                    taskId: element.task.id,
-                    taskName: element.task.name
-                },
-                icon: {
-                    path: 'src/icons/start.png'
-                }
-            }));
 
-        alfy.output(items);
-    })
-    .catch(error => {
-        alfredError(error, 'Failed to list project tasks.');
-    });
+const items = alfy
+    .inputMatches( JSON.parse( projectTasks ), 'task.name')
+    .filter(element => element.is_active)
+    .map(element => ({
+        uid: element.id,
+        title: element.task.name,
+        subtitle: 'Start this task',
+        variables: {
+            taskId: element.task.id,
+            taskName: element.task.name
+        },
+        icon: {
+            path: 'src/icons/start.png'
+        }
+    }));
+
+alfy.output(items);
